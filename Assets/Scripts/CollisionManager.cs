8f4cc9d5de4cb01e9dc8f8765da4e668bfdbb838
Ebru Çakar <ebru_cakar_11@hotmail.com>
2021-10-13 17:07:31 +0300

    Initial commit

---
+++ Assets/Scripts/CollisionManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionManager : MonoBehaviour
+{
+    public List<GameObject> MoneyPiles;
+    int currentId = 0;
+
+    private void Update()
+    {
+        for (int i = 0; i < MoneyPiles.Count; i++)
+        {
+            
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("MoneyGroup"))
+        {
+            other.gameObject.SetActive(false);
+            MoneyPiles[currentId].SetActive(true);
+            currentId++;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Dreamteck.Splines;
+using DG.Tweening;
+
+public class PlayerController : MonoBehaviour
+{
+    Vector3 move;
+    public CharacterController controller;
+
+    private Touch touch;
+    public SplineFollower splineFollower;
+    public float xSpeed = 15, zSpeed = 10;
+
+    void Update()
+    {
+#if UNITY_EDITOR
+        if (Input.GetMouseButtonDown(0))
+        {
+            //splineFollower.followSpeed = xSpeed;
+        }
+        else if (Input.GetMouseButton(0))
+        {
+            Vector3 move = controller.transform.right * Input.GetAxis("Mouse X") * xSpeed;
+
+            controller.Move(move * Time.deltaTime);
+            controller.transform.localPosition = new Vector3(Mathf.Clamp(controller.transform.localPosition.x, -3.5f, 3.5f), 0, 0);
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            splineFollower.followSpeed = 0;
+        }
+
+#elif UNITY_IOS || UNITY_ANDROID
+
+        if (Input.touchCount > 0)
+        {
+            touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Began)
+            {
+                splineFollower.followSpeed = xSpeed;
+            }
+            else if (touch.phase == TouchPhase.Moved)
+            {
+                move += controller.transform.right * Input.GetAxis("Mouse X") * xSpeed;
+            }
+            else if (touch.phase == TouchPhase.Ended)
+            {
+                splineFollower.followSpeed = 0;
+            }
+            controller.Move(move * Time.deltaTime);
+            controller.transform.localPosition = new Vector3(Mathf.Clamp(controller.transform.localPosition.x, -3.5f, 3.5f), 0, 0);
+        }
+#endif
+    }
+}
